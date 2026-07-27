e21354fbae1c05edbbaba0800763f3bed291630c
jalmazanrobledo <jalmazanbarrios@gamil.com>
2026-07-27 11:41:41 -0600

    mejora alumno_2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Desactiva el empaquetador experimental Rolldown y usa Rollup estándar
+    rolldown: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
