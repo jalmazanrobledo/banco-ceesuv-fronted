cfbb96f8ef1630cbd76550d7df687969a22c63a5
jalmazanrobledo <jalmazanbarrios@gamil.com>
2026-07-28 15:45:46 -0600

    Cache_1

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Cache-Control", "Pragma", "Expires"]
   })
 );
 
