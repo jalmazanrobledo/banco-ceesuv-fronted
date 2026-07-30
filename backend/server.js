ded0dac57d2cd340a717b328fa60248e2c09f1bd
jalmazanrobledo <jalmazanbarrios@gamil.com>
2026-07-30 15:03:26 -0600

    Transferencias_17

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 
 // Middleware explícito para asegurar cabeceras en Vercel
 app.use((req, res, next) => {
